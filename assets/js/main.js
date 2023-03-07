c046f6c33ab256a9fcb6d366ca3456c7b31ec175
giuseppesciacca <sciaccagi@gmail.com>
2023-03-07 15:33:26 +0100

    [initial commit] js-pwdgen-wannabe

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+*/
