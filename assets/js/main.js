a749e206d136ad02f700dd7108e7134887dd4496
giuseppesciacca <sciaccagi@gmail.com>
2023-03-07 15:52:56 +0100

    [add] variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,15 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
 */
+
+let name = prompt('Inserisci il tuo nome qui:');
+
+let surname = prompt('Inserisci il tuo cognome qui:');
+
+let color = prompt('Quale è il tuo colore preferito?');
+
+
+
+
+
+
